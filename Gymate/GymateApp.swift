424b83dd8332bf0d7c96ef014a2555b4fea97205
xngsoo <hanxngsoo@gmail.com>
2024-03-26 13:31:49 +0900

    create project

---
+++ Gymate/GymateApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymateApp.swift
+//  Gymate
+//
+//  Created by Han on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct GymateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
